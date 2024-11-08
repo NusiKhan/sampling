df79d1822e88a93b512ea2cdfc01b2c9a2629006
NusiKhan <nfk21@outlook.com>
2024-11-08 15:46:30 -0600

    Set random seed and number of simulations

--- 02_activities/assignments/whitby_covid_tracing.py
+++ 02_activities/assignments/whitby_covid_tracing.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 # Note: Suppressing FutureWarnings to maintain a clean output. This is specifically to ignore warnings about
 # deprecated features in the libraries we're using (e.g., 'use_inf_as_na' option in Pandas, used by Seaborn),
 # which we currently have no direct control over. This action is taken to ensure that our output remains
@@ -14,8 +15,11 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Constants representing the parameters of the model
 ATTACK_RATE = 0.10
+
 TRACE_SUCCESS = 0.20
 SECONDARY_TRACE_THRESHOLD = 2
+
+
 
 def simulate_event(m):
   """
@@ -67,8 +71,13 @@ def simulate_event(m):
 
   return p_wedding_infections, p_wedding_traces
 
-# Run the simulation 50000 times
-results = [simulate_event(m) for m in range(50000)]
+# Run the simulation 1000 times
+#Set Random Seed 
+
+np.random.seed(42)
+
+
+results = [simulate_event(m) for m in range(1000)]
 props_df = pd.DataFrame(results, columns=["Infections", "Traces"])
 
 # Plotting the results
